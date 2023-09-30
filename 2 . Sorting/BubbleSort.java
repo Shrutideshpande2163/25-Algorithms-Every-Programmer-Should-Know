36d2690e350692b213743b8e54d9a57d7afa8759
Shrutideshpande2163 <146514326+Shrutideshpande2163@users.noreply.github.com>
2023-10-01 00:16:11 +0530

    BubbleSort.java

--- 2 . Sorting/BubbleSort.java
+++ 2 . Sorting/BubbleSort.java
@@ -16,6 +16,8 @@ public class BubbleSort {
                     swapped = true;
                 }
             }
+
+            
             
             // If no two elements were swapped in the inner loop, the array is already sorted
             if (!swapped) {
@@ -45,31 +47,24 @@ public class BubbleSort {
     }
 }
 
+/* BubbleSort Class:
 
-/* Here's an explanation of the key parts of the program:
+This class contains two methods: sortArray and main.
+sortArray Method:
 
-bubbleSort method:
+The sortArray method takes an integer array array as input and sorts it in ascending order using the Bubble Sort algorithm.
+It uses two nested for loops. The outer loop iterates from i = 0 to i = array.length - 2. This loop is responsible for controlling the number of passes required to sort the array.
 
-The bubbleSort method takes an integer array arr as its input and sorts it in-place using the Bubble Sort algorithm.
-It starts by initializing a variable n with the length of the input array arr.
-It also initializes a boolean variable swapped to keep track of whether any swaps were made in the current pass through the array.
-Outer Loop:
+The inner loop iterates from j = 0 to j = array.length - i - 2. The -i part is because after each pass, the largest element is bubbled up to the end of the array, so there is no need to compare and swap it again.
+Inside the inner loop, it compares array[j] and array[j+1]. If array[j] is greater than array[j+1], it swaps them to ensure that the larger element "bubbles up" to the correct position.
+This process continues for each pass until the entire array is sorted.
+main Method:
 
-The outer loop iterates from i = 0 to i < n - 1. This loop represents the number of passes through the array.
-Inner Loop:
+The main method is the entry point of the program.
+It initializes an integer array array with unsorted values from 8 down to 0.
+It then calls the sortArray method to sort the array in ascending order.
+After sorting, it iterates through the sorted array and prints each element on the same line with a space between them.
+Overall Functionality:
 
-The inner loop, nested within the outer loop, iterates from j = 0 to j < n - i - 1. It compares adjacent elements of the array and swaps them if they are in the wrong order (i.e., if arr[j] > arr[j + 1]).
-If a swap is made during this inner loop, the swapped variable is set to true.
-Check for Early Termination:
-
-After each pass through the array, the program checks the swapped variable. If no swaps were made in the inner loop (i.e., swapped is still false), it means the array is already sorted, and the outer loop can be terminated early using the break statement.
-main method:
-
-In the main method, an example integer array arr is created with unsorted values.
-The original array is printed using the printArray method.
-The bubbleSort method is called to sort the array.
-Finally, the sorted array is printed again using the printArray method to show the result.
-printArray method:
-
-The printArray method takes an integer array arr as input and prints its elements on a single line, separated by spaces.
-When you run the program, it will output the original array, perform the Bubble Sort algorithm on it, and then display the sorted array as the final result. */
+The program demonstrates the Bubble Sort algorithm by sorting an array of integers in ascending order.
+After sorting, it prints the sorted array.*/
